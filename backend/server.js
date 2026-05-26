2152f41fe6a03baf59bc9dcfb9b5a926448504bf
Sammyzain48 <samuelnwachukwu720@gmail.com>
2026-05-26 07:24:47 +0100

    had to change some api settings

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 require("dotenv").config({
-  path: "../backend/expense-tracker-backend/.env",
+  path: "./expense-tracker-backend/.env",
 });
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectToDatabase = require("./expense-tracker-backend/db/connectToDB");
-const authRoute = require("../backend/expense-tracker-backend/routes/auth-routes");
+const authRoute = require("./expense-tracker-backend/routes/auth-routes");
 const dashboardRoute = require("./expense-tracker-backend/routes/dashboard-routes");
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
@@ -28,5 +28,5 @@ const PORT = process.env.PORT || 3000;
 connectToDatabase();
 
 app.listen(PORT, () => {
-  console.log(`server is running at port ${PORT} `);
+  console.log(`server is running at port ${PORT}`);
 });
