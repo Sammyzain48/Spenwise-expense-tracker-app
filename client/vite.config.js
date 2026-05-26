c7a0d92b7ff5b8a11cdd0fb0be5609a3b8d1f9cf
Sammyzain48 <samuelnwachukwu720@gmail.com>
2026-05-26 08:58:57 +0100

    fix _redirects for netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",
-    },
-  },
+  publicDir: "public",
 });
